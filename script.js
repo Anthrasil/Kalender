ca778e02c66ea18897bf5ae0628121e89f830341
Anthrasil <raphaelchristopher.dorninger@gmail.com>
2024-09-15 09:30:17 +0200

    made the basic ui

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-createWeekUI()
-function createWeekUI() {
-    let HoursAtADay=24
-    let DaysInAWeek=7
-    let weekMonitor=document.querySelector(".week")
-    let DayNames=[
+createWeek()
+function createWeek() {
+    let weekMonitor = document.querySelector(".week")
+    let timeOut = false
+    let Termine = [
+        {
+            x: 0,
+            y: 1,
+            color: "lightblue",
+        }
+    ]
+    createWeekUI(weekMonitor, Termine)
+    while (true && !timeOut) {
+        createWeekEvents(weekMonitor, Termine)
+        let timeOutTimeInMS = 10
+        setTimeout(function () { timeOut = false }, timeOutTimeInMS);
+        timeOut = true
+    }
+}
+function createWeekUI(weekMonitor, Termine) {
+    weekMonitor.innerHTML = ""
+    let HoursAtADay = 24
+    let DaysInAWeek = 7
+    let DayNames = [
         "Monday",
         "Tuesday",
         "Wednesday",
@@ -12,48 +30,100 @@ function createWeekUI() {
         "Saturday",
         "Sunday"
     ]
-    let time=createHtmlelement("div","time")
-    for (let timeInHalfHours=0; timeInHalfHours<HoursAtADay;timeInHalfHours+=0.5) {
-        let onTimeElement=createHtmlelement("div","timeElement");
-        let Minute=0
-        let timecorrect=0
-        let zerobefore=""
-        if (timeInHalfHours%1!=0) {
-            Minute=3
-            timecorrect=0.5
+    let time = createHtmlelement("div", "time")
+    let blanksell = createHtmlelement("h1", "head")
+    blanksell.innerText = "blank"
+    blanksell.style.color = "transparent"
+    time.appendChild(blanksell)
+    for (let timeInHalfHours = 0; timeInHalfHours < HoursAtADay; timeInHalfHours += 0.5) {
+        let Minute = 0
+        let timecorrect = 0
+        let zerobefore = ""
+        let id = ""
+        let color = ""
+        Termine.forEach((el) => {
+            if (el.x != 0) {
+                return
+            }
+            if (el.y != timeInHalfHours * 2) {
+                return
+            }
+            color = el.color
+        })
+        if (timeInHalfHours % 1 != 0) {
+            Minute = 3
+            timecorrect = 0.5
+            if (timeInHalfHours - timecorrect == 23) {
+                id = "last"
+            }
         }
-        let Hour=timeInHalfHours-timecorrect
-        if (Hour<10) {
-            zerobefore="0"
+        let oneTimeElement = createHtmlelement("div", `timeElement ${id}`);
+        let Hour = timeInHalfHours - timecorrect
+        if (Hour < 10) {
+            zerobefore = "0"
         }
-        onTimeElement.innerText=`${zerobefore}${Hour}:${Minute}0`;
-        time.appendChild(onTimeElement);
+        oneTimeElement.innerText = `${zerobefore}${Hour}:${Minute}0`;
+        oneTimeElement.style.backgroundColor = color
+        time.appendChild(oneTimeElement);
     }
     weekMonitor.appendChild(time);
-    for (let day=0; day<DaysInAWeek;day++) {
-        let Day=createHtmlelement("div","day",DayNames[day])
-        let head=createHtmlelement("h1","head");
-        head.innerText=DayNames[day]
-        Day.append(head)
-        for (let timeInHalfHours=0; timeInHalfHours<HoursAtADay;timeInHalfHours+=0.5) {
-            let element=createHtmlelement("div","box")
+    for (let day = 0; day < DaysInAWeek; day++) {
+        let Day = createHtmlelement("div", "day", DayNames[day])
+        let head = createHtmlelement("h1", "head");
+        head.innerText = DayNames[day]
+        Day.appendChild(head)
+        for (let timeInHalfHours = 0; timeInHalfHours < HoursAtADay; timeInHalfHours += 0.5) {
+            let id = ""
+            if (timeInHalfHours == 23.5) {
+                id = "last"
+            }
+            let element = createHtmlelement("div", `box ${id}`)
             Day.appendChild(element)
         }
         weekMonitor.appendChild(Day)
     }
 }
-function createHtmlelement(element,cla="",id="") {
+function createWeekEvents(/**@type {HTMLElement} */weekMonitor, Termine) {
+    let Event = {
+        type: "",
+        x: 0,
+        y: 0,
+    }
+    let children = []
+    Object.keys(weekMonitor.children).forEach((num) => {
+        children.push(weekMonitor.children[num])
+    })
+    children.forEach((/**@type {HTMLElement} */el, index) => {
+        let children = []
+        Object.keys(el.children).forEach((num) => {
+            children.push(el.children[num])
+        })
+        el.addEventListener("click", (event) => {
+            Event.x = index
+            children.forEach((el, index) => {
+                el.addEventListener("click", (event) => {
+                    Event.y = index
+                })
+            })
+        })
+    })
+    document.addEventListener("click", function () {
+        console.log(weekMonitor.children[Event.x].children[Event.y])
+        createWeekUI(weekMonitor, Termine)
+    })
+}
+function createHtmlelement(element, cla = "", id = "") {
     if (!element) {
         throw new Error("A element is requiered");
         return;
     }
     /**@type {HTMLElement} */
-    let el=document.createElement("div");
+    let el = document.createElement("div");
     if (cla) {
-        el.className=cla;
+        el.className = cla;
     }
     if (id) {
-        el.id=id;
+        el.id = id;
     }
     return el;
 }
